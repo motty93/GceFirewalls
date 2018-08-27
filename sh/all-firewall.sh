6ad383b7c6c48bbabbf6545a70598b16c97b675c
motty93 <rdwbocungelt5@gmail.com>
2018-08-27 17:40:49 +0900

    deal with OSX

--- sh/all-firewall.sh
+++ sh/all-firewall.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-count=1
+count=0
 BASE_DIRECTORY=$(cd $(dirname $0); pwd)
 DIR_NAME=$(dirname ${BASE_DIRECTORY})
 
@@ -8,9 +8,15 @@ while read line
 do
   if [ -s $DIR_NAME/iptext/all-ip-addresses.txt ]; then
     count=$(( count + 1 ))
-    ADDRESSES=`head -n 255 $DIR_NAME/iptext/all-ip-addresses.txt | sed -e ':loop; N; $!b loop; s/[\r\n]\+/,/g'`
+    if [ "$(uname)" == 'Linux' ]; then
+      ADDRESSES=`head -n 255 $DIR_NAME/iptext/all-ip-addresses.txt | sed -e ':loop; N; $!b loop; s/[\r\n]\+/,/g'`
+    elif [ "$(uname)" == 'Darwin' ]; then
+      ADDRESSES=`head -n 255 $DIR_NAME/iptext/all-ip-addresses.txt | sed "" -e ':loop; N; $!b loop; s/[\r\n]\+/,/g'`
+    else
+      ADDRESSES=""
+    fi
     sed -i -e 1,255d $DIR_NAME/iptext/all-ip-addresses.txt
-    gcloud compute firewall-rules create ip-reject-$count \
+    gcloud compute firewall-rules create ip-reject-0$count \
             --priority 10 \
             --action deny \
             --direction ingress \
