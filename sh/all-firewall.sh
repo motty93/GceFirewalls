3b43606d436ee2c6476b74c52fefbd9bbffd9511
motty93 <rdwbocungelt5@gmail.com>
2018-08-27 17:26:26 +0900

    Directory constitution changed & completed.

--- sh/all-firewall.sh
+++ sh/all-firewall.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
-COUNT=1
+count=1
 BASE_DIRECTORY=$(cd $(dirname $0); pwd)
 DIR_NAME=$(dirname ${BASE_DIRECTORY})
-echo DIR_NAME
 
 while read line
 do
   if [ -s $DIR_NAME/iptext/all-ip-addresses.txt ]; then
-    ADDRESSES=`head -n 255 $DIR_NAME/iptext/all-ip-philippines.txt | sed -e ':loop; N; $!b loop; s/[\r\n]\+/,/g'`
+    count=$(( count + 1 ))
+    ADDRESSES=`head -n 255 $DIR_NAME/iptext/all-ip-addresses.txt | sed -e ':loop; N; $!b loop; s/[\r\n]\+/,/g'`
     sed -i -e 1,255d $DIR_NAME/iptext/all-ip-addresses.txt
-    echo "gcloud compute firewall-rules create ip-block-$COUNT \
+    gcloud compute firewall-rules create ip-reject-$count \
             --priority 10 \
             --action deny \
             --direction ingress \
             --rules all \
-            --source-ranges $ADDRESSES"
-    $COUNT=`expr $COUNT + 1`
+            --source-ranges $ADDRESSES
   fi
 done < $DIR_NAME/iptext/all-ip-addresses.txt
 
-git checkout $DIR_NAME/iptext/all-ip-addresses.txt
+cd $DIR_NAME
+git checkout iptext/all-ip-addresses.txt
--- sh/test.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-SCRIPT_DIR=$(cd $(dirname $0); pwd)
-echo $(dirname ${SCRIPT_DIR})
