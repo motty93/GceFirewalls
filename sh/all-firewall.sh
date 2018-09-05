8c5438a7c6f11a2c0ee74e3b2619bbf0529d70b8
motty93 <rdwbocungelt5@gmail.com>
2018-09-05 15:18:35 +0900

    code refactoring

--- sh/all-firewall.sh
+++ sh/all-firewall.sh
@@ -2,37 +2,39 @@
 
 count=0
 BASE_PATH=$(cd $(dirname $0); pwd)
-DIR_PARH=$(dirname ${BASE_PATH})
+DIR_PATH=$(dirname ${BASE_PATH})
+IP_TEXT_PATH=$DIR_PATH/iptext/all-ip-addresses.txt
+
+function GcloudFirewall() {
+  gcloud compute firewall-rules create ip-reject-$1 \
+    --priority 10 \
+    --action deny \
+    --direction ingress \
+    --rules all \
+    --source-ranges $2
+}
 
 while read line
 do
-  if [ -s $DIR_PATH/iptext/all-ip-addresses.txt ]; then
+  if [ -s $IP_TEXT_PATH ]; then
     count=$(( count + 1 ))
     if [ "$(uname)" == 'Linux' ]; then
-      ADDRESSES=`head -n 255 $DIR_PATH/iptext/all-ip-addresses.txt | sed -e ':loop; N; $!b loop; s/[\r\n]\+/,/g'`
+      ADDRESSES=`head -n 255 $IP_TEXT_PATH | sed -e ':loop; N; $!b loop; s/[\r\n]\+/,/g'`
     elif [ "$(uname)" == 'Darwin' ]; then
-      ADDRESSES=`head -n 255 $DIR_PATH/iptext/all-ip-addresses.txt | sed "" -e ':loop; N; $!b loop; s/[\r\n]\+/,/g'`
+      ADDRESSES=`head -n 255 $IP_TEXT_PATH | sed "" -e ':loop; N; $!b loop; s/[\r\n]\+/,/g'`
     else
-      ADDRESSES=""
+      echo 'Windows cannot use this shell script.' && exit 0
     fi
-    sed -i -e 1,255d $DIR_PATH/iptext/all-ip-addresses.txt
+
+    sed -i -e 1,255d $IP_TEXT_PATH
+
     if [ $count -lt 10 ]; then
-      gcloud compute firewall-rules create ip-reject-0$count \
-              --priority 10 \
-              --action deny \
-              --direction ingress \
-              --rules all \
-              --source-ranges $ADDRESSES
+      GcloudFirewall 0$count $ADDRESSES
     else
-      gcloud compute firewall-rules create ip-reject-$count \
-              --priority 10 \
-              --action deny \
-              --direction ingress \
-              --rules all \
-              --source-ranges $ADDRESSES
+      GcloudFirewall $count $ADDRESSES
     fi
   fi
-done < $DIR_PATH/iptext/all-ip-addresses.txt
+done < $IP_TEXT_PATH
 
 cd $DIR_PATH
 git checkout iptext/all-ip-addresses.txt
